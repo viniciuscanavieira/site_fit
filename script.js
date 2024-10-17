8630626a307173f7ac0306d9bcd4de39305e09a8
Vinicius <vinicius.teixeirapix@gmail.com>
2024-10-17 19:55:26 -0300

    atualizacao navbar, footer e img

--- script.js
+++
@@ -1,7 +0,0 @@
-function openPopup(popupId) {
-    document.getElementById(popupId).style.display = 'flex';
-}
-
-function closePopup(popupId) {
-    document.getElementById(popupId).style.display = 'none';
-}
