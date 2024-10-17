173d80c805631864b34f1974811868e0dbcea709
Vinicius <vinicius.teixeirapix@gmail.com>
2024-10-17 18:58:01 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+function openPopup(popupId) {
+    document.getElementById(popupId).style.display = 'flex';
+}
+
+function closePopup(popupId) {
+    document.getElementById(popupId).style.display = 'none';
+}
